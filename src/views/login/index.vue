57bb551f624385530eaeefdd293be2f3a2babf2b
oneicecream <w15886761501@163.com>
2019-07-09 17:31:43 +0800

    登录-页面布局&&封装axios请求模块

---
+++ src/views/login/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <!-- 头部 -->
+    <van-nav-bar
+      title="标题"
+    />
+    <!-- /头部 -->
+    <!-- 表单 -->
+    <form>
+      <van-cell-group>
+        <van-field
+          v-model="user.mobile"
+          required
+          clearable
+          label="手机号"
+          placeholder="请输入手机号"
+        />
+
+        <van-field
+          v-model="user.code"
+          type="password"
+          label="密码"
+          placeholder="请输入密码"
+          required
+        />
+      </van-cell-group>
+      <div class="login-btn-box">
+        <van-button type="info" class="login-btn">登录</van-button>
+      </div>
+    </form>
+    <!-- /表单 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-btn-box {
+  padding: 15px;
+  .login-btn {
+    width: 100%;
+  }
+}
+</style>
