9bd4773d49cf4abab9bd75a59b8c2035065244a9
oneicecream <w15886761501@163.com>
2019-07-11 19:36:32 +0800

    登录-配置表单验证

--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import store from '@/store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import 'amfe-flexible'
+import VeeValidate, { Validator } from 'vee-validate'
+import zhCN from 'vee-validate/dist/locale/zh_CN'
 
+Vue.use(VeeValidate, {
+  // events: 'change'
+})
+Validator.localize('zh-CN', zhCN)
 Vue.use(Vant)
 
 Vue.config.productionTip = false
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -14,7 +14,9 @@
           clearable
           label="手机号"
           placeholder="请输入手机号"
-          :error-message="errors.mobile"
+          v-validate="'required'"
+          name='mobile'
+          :error-message="errors.first('mobile')"
         />
 
         <van-field
@@ -22,8 +24,10 @@
           type="password"
           label="密码"
           placeholder="请输入密码"
-          :error-message="errors.code"
           required
+          v-validate="'required'"
+          name='code'
+          :error-message="errors.first('code')"
         />
       </van-cell-group>
       <div class="login-btn-box">
@@ -46,35 +50,33 @@ export default {
         code: '123456'
       },
       loginLoading: false, // 控制登录按钮的 loading 状态
-      errors: {
+      myErrors: {
         mobile: '',
         code: ''
       }
     }
   },
+
+  created () {
+    this.configFormErrorMessages()
+  },
+
   methods: {
     async handleLogin () {
       try {
-        const { mobile, code } = this.user
-        const errors = this.errors
+        // 调用JS触发验证
+        const valid = await this.$validator.validate()
 
-        if (mobile.length) {
-          errors.mobile = ''
-        } else {
-          errors.mobile = '手机号不能为空'
+        // 如果校验失败，则停止后续代码执行
+        if (!valid) {
           return
         }
 
-        if (code.length) {
-          errors.mobile = ''
-        } else {
-          errors.code = '验证码不能为空'
-          return
-        }
-
+        // 表单验证通过，发送请求，loading 加载
         this.loginLoading = true
 
         const data = await login(this.user)
+
         this.$store.commit('setUser', data)
         // 这里先简单粗暴的跳转到首页
         // 真实的业务要处理成跳转到之前过来的页面
@@ -82,10 +84,29 @@ export default {
           name: 'home'
         })
       } catch (err) {
-        console.log('err')
-        console.log('登录失败')
+        this.$toast('登录失败')
       }
       this.loginLoading = false
+    },
+    configFormErrorMessages () {
+      const dict = {
+        custom: {
+          mobile: {
+            required: '手机号不能为空'
+            // mobile: '请输入一个有效的手机号'
+          },
+          code: {
+            required: '密码不能为空'
+            // code: '请输入一个有效的密码'
+          }
+        }
+      }
+      // 如果需要错误消息提示全局生效
+      // Validator.localize('en', dict)
+
+      // 组建中这样注册生效
+      // or use the instance method
+      this.$validator.localize('zh-CN', dict)
     }
   }
 }
