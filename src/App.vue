9e2213deefededde3f4da79d1843094d0a2ab009
oneicecream <w15886761501@163.com>
2019-07-13 15:12:04 +0800

    处理路由缓存之后重新获取频道数据

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       如果想要在切换的时候保留状态，说白了不要销毁，则使用 keep-alive 缓存就可以了
       参考文档：https://cn.vuejs.org/v2/guide/components-dynamic-async.html
      -->
-    <router-view ></router-view>
+    <router-view />
   </keep-alive>
 </template>
 
