6cfbdeb7870af2426d1e1279e5a134da770072c3
oneicecream <w15886761501@163.com>
2019-07-14 21:56:32 +0800

    配置解决后端返回数据超出js安全整数范围

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import store from '@/store'
+import JSONbig from 'json-bigint'
 
 // axios.create 用于创建一个 axios 实例，该实例和 axios 的功能是一模一样的
 // 说白了就是克隆了一个 axios
@@ -10,6 +11,19 @@ const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/'
   // baseURL: 'http://toutiao.course.itcast.cn'
 })
+
+/**
+ * 处理后端返回数据中数字超出 JavaScript 安全整数范围
+ */
+request.defaults.transformResponse = [function (data) {
+  try {
+    // 如果是 json 格式字符串，那就转换并返回给后续使用
+    return JSONbig.parse(data)
+  } catch (err) {
+    // 报错就意味着 data 不是 json 格式字符串，这里就直接原样返回给后续使用
+    return data
+  }
+}]
 
 // 拦截器
 // Add a request interceptor
