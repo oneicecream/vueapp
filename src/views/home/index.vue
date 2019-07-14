78151999d000782ab3c385cc47641f86817c38ea
oneicecream <w15886761501@163.com>
2019-07-14 21:29:36 +0800

    文章列表更多操作-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -60,6 +60,7 @@
                     局部：filters 选项，只能在组件内部使用
                  -->
                 <span>{{ item.pubdate | relativeTime }}</span>
+                <van-icon class="close" name="close" />
             </p>
             </div>
 
@@ -88,6 +89,27 @@
     :active-index.sync="activeChannelIndex"
     />
     <!-- /频道组件 -->
+
+    <!-- 更多操作弹框 -->
+    <van-dialog
+      v-model="isMoreActionShow"
+      :showConfirmButton="false"
+    >
+      <van-cell-group v-if="!toggleRubbish">
+        <van-cell icon="arrow-left" />
+        <van-cell title="不感兴趣" />
+        <van-cell title="反馈垃圾内容" is-link @click="toggleRubbish = true" />
+        <van-cell title="拉黑作者" />
+      </van-cell-group>
+      <van-cell-group v-else>
+        <van-cell icon="arrow-left" @click="toggleRubbish = false" />
+        <van-cell title="标题夸张" />
+        <van-cell title="低速色情" />
+        <van-cell title="错别字多" />
+        <van-cell title="旧闻重复" />
+      </van-cell-group>
+    </van-dialog>
+    <!-- /更多操作弹框 -->
   </div>
 </template>
 
@@ -109,7 +131,9 @@ export default {
       loading: false,
       finished: false,
       pullRefreshLoading: false,
-      isChannelShow: false
+      isChannelShow: false, // 控制频道面板的显示状态
+      isMoreActionShow: true, // 控制更多操作弹框面板
+      toggleRubbish: false // 控制反馈垃圾弹框内容的显示
     }
   },
 
@@ -312,5 +336,9 @@ export default {
   align-items: center;
   background: #fff;
   opacity: .7;
+}
+.channel-tabs .close {
+  float: right;
+  font-size: 30px;
 }
 </style>
