dacd461f77eeaa788f575698fbd2774288055d63
oneicecream <w15886761501@163.com>
2019-07-15 19:46:43 +0800

    搜索-联想建议-基本完成

---
+++ src/api/search.js
@@ -0,0 +1,15 @@
+// 搜索相关接口
+
+import request from '@/utils/request'
+
+// 获取联想建议 (自动补全)
+
+export const getSuggestion = q => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/suggestion',
+    params: {
+      q
+    }
+  })
+}
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -7,19 +7,17 @@
     />
     <!-- 联想建议列表 -->
     <van-cell-group>
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
+      <van-cell
+        icon="search"
+        v-for="item in suggestions"
+        :key="item"
+        :title="item"
+      />
     </van-cell-group>
     <!-- /联想建议列表 -->
 
     <!-- 历史记录 -->
-    <van-cell-group>
+    <!-- <van-cell-group>
       <van-cell title="历史记录">
         <van-icon
           slot="right-icon"
@@ -27,17 +25,36 @@
           style="line-height: inherit;"
         />
       </van-cell>
-    </van-cell-group>
+    </van-cell-group> -->
     <!-- /历史记录 -->
   </div>
 </template>
 
 <script>
+import { getSuggestion } from '@/api/search'
+
 export default {
   name: 'SearchIndex',
+
   data () {
     return {
-      searchText: ''
+      searchText: '',
+      suggestions: []
+    }
+  },
+
+  watch: {
+    async searchText (newVal) {
+      newVal = newVal.trim() // 去除首尾空格
+
+      // 如果数据为空，则什么都不做
+      if (!newVal) {
+        return
+      }
+
+      // 如果数据不为空，则请求联想建议自动补全
+      const data = await getSuggestion(newVal)
+      this.suggestions = data.options
     }
   }
 }
