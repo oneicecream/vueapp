5dfd476f6b7bf35e0282654693c0fbd92d34b247
oneicecream <w15886761501@163.com>
2019-07-17 17:49:16 +0800

    搜索-导航搜索结果页面

---
+++ src/views/search-result/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchResult',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
