3c331c4be0669fa87c7dc6cabf761c7e71ea9b90
oneicecream <w15886761501@163.com>
2019-07-17 19:16:29 +0800

    搜索结果-准备页面结构

--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -1,20 +1,74 @@
 <template>
-  <div>
+  <div class="search-result">
+    <!-- 顶部导航 -->
+    <van-nav-bar
+      title="搜索结果"
+      left-text="返回"
+      left-arrow
+      fixed
+    />
+    <!-- 顶部导航 -->
 
+    <!-- 文章列表 -->
+    <van-list
+      class="article-list"
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell
+        v-for="item in list"
+        :key="item"
+        :title="item"
+      />
+    </van-list>
+    <!-- /文章列表 -->
   </div>
 </template>
 
 <script>
+import { getSearch } from '@/api/search'
 export default {
   name: 'SearchResult',
   data () {
     return {
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
 
+  async created () {
+    const data = await getSearch({
+      q: this.$route.params.q
+    })
+    console.log(data)
+  },
+
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.search-result {
+  .article-list {
+    margin-top: 92px;
+  }
+}
 </style>
