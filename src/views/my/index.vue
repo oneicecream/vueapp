69ae0c8209d4267e092a9e471e64e6df7efd59cb
oneicecream <w15886761501@163.com>
2019-07-20 16:13:21 +0800

    我的模块-根据登录状态展示不同组件

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
+    <UnLogin v-if="!$store.state.user"/>
+    <UserInfo v-else/>
     <app-tabbar></app-tabbar>
   </div>
 </template>
 
 <script>
+import UnLogin from './components/un-login'
+import UserInfo from './components/user-info'
+
 export default {
   name: 'MyIndex',
+  components: {
+    UnLogin,
+    UserInfo
+  },
   data () {
     return {
 
